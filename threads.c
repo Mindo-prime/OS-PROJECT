b4ed726305956e32a68232a25c2b2dc2fde598cc
AhmedHAEM <ahmedhaem2004@gmail.com>
2025-03-04 14:51:00 +0200

    Finished second thread and its function.

--- threads.c
+++ threads.c
@@ -21,10 +21,19 @@ void* thread1(void* arg) {
     printf("\n");
 }
 
+void* thread2(void* args) {
+    printf("Thread 2 ID: %lu\n", pthread_self());
+    printf("This statement indicated that thread 2 is executing.\n");
+    printf("Thread 2 will now finish execution\n");
+}
+ 
 int main() {
     pthread_t t1, t2, t3;
 
     pthread_create(&t1, NULL, thread1, NULL);
+    pthread_create(&t2, NULL, thread2, NULL);
+
     pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
     
 }
