bbb80205312269b1729eef72381577c9493ce536
AhmedHAEM <ahmedhaem2004@gmail.com>
2025-03-05 11:40:27 +0200

    Finished third thread and its functions.

--- threads.c
+++ threads.c
@@ -26,14 +26,31 @@ void* thread2(void* args) {
     printf("This statement indicated that thread 2 is executing.\n");
     printf("Thread 2 will now finish execution\n");
 }
- 
+
+void* thread3(void* args) {
+    int a, b;
+
+    printf("Please input 2 integers.");
+    scanf("%c %c", &a, &b);
+
+    int sum = a + b;
+    printf("The sum of the two integers is: %d", sum);
+
+    float average = sum/2;
+    printf("The average of the two integers is: %.1f", average);
+
+    int product = a*b;
+    printf("The product of the two integers is %d", product);
+}
+
 int main() {
     pthread_t t1, t2, t3;
 
     pthread_create(&t1, NULL, thread1, NULL);
     pthread_create(&t2, NULL, thread2, NULL);
+    pthread_create(&t3, NULL, thread3, NULL);
 
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
-    
+    pthread_join(t3, NULL);
 }
